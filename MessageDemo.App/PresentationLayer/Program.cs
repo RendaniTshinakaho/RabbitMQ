224f5159a830a5a7727e65db76ad6512e4662605
Rendani <r.nkhumeleni@gmail.com>
2014-03-10 14:58:54 +0200

    Console appl setting up rabbitmq

---
+++ MessageDemo.App/PresentationLayer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace PresentationLayer
+{
+    class Program
+    {
+        private const string HostName = "localhost";
+        private const string UserName = "guest";
+        private const string Password = "guest";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting RabbitMQ Queue Creator");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var connectionFactory = new RabbitMQ.Client.ConnectionFactory(){Password = Password,UserName = UserName,HostName = HostName};
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+            model.QueueDeclare("IncomingQueue",true,false,false,null);
+            Console.WriteLine("Queue created");
+            model.ExchangeDeclare("MySecondExchange",ExchangeType.Topic);
+            Console.WriteLine("Exchange created...");
+            model.QueueBind("IncomingQueue","MySecondExchange","cars");
+            Console.WriteLine("Exchange and Queue bound");
+            Console.ReadLine();
+        }
+    }
+}
